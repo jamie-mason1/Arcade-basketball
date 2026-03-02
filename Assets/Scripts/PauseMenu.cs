8c75332128278fe338102b3731ef233127cf7570
Jamie awesome <jamie.mason@ontariotechu.net>
2026-03-01 20:45:43 -0500

    working pause menu buttons

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        if (GameIsPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                LoadMenu();
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                QuitGame();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 4f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None; // unlock cursor
+        Cursor.visible = true;     
+    }
+
+    public void LoadMenu()
+
+    {
+        Time.timeScale = 4f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
